14273bd9b6906fc683b350c85d11adf1a4b0c934
eastlife <dengbx1212@163.com>
2019-01-09 10:46:21 -0500

    server connected with mongoDB(mlab)

--- oj-server/services/problemService.js
+++ oj-server/services/problemService.js
@@ -1,3 +1,5 @@
+
+
 var problems = [
     {
         id: 1,
@@ -39,26 +41,43 @@ var ProblemModel = require("../models/problemModel");
 
 var getProblems = function() {
     return new Promise((resolve, reject) => {
-        resolve(problems);
+        ProblemModel.find({}, function (err, problems) {
+            if (err){
+                reject(err);
+            } else {
+                resolve(problems);
+            }
+        });
     });
 }
 
 var getProblem = function(id) {
     return new Promise((resolve, reject) => {
-        resolve(problems.find(problem => problem.id === id));
+        ProblemModel.findOne({ id: id }, function (err, problem) {
+            if (err){
+                reject(err);
+            } else {
+                resolve(problem);
+            }
+        });
     });
 }
 
 var addProblem = function(newProblem){
     return new Promise((resolve, reject) => {
-        if (problems.find(problem => problem.name === newProblem.name)){
-            reject("Problem exists");
-        } else {
-            newProblem.id = problems.length + 1;
-            problems.push(newProblem);
-            resolve(newProblem);
-        }
-    })
+        ProblemModel.findOne({ name: newProblem }, function (err, problem) {
+            if (problem){
+                reject("Problem name already exists");
+            } else {
+                ProblemModel.count({}, function (err, num){
+                    newProblem.id = num + 1;
+                    var mongoProblem = new ProblemModel(newProblem);
+                    mongoProblem.save();
+                    resolve(newProblem);
+                });
+            }
+        });
+    });
 }
 
 module.exports = {
