7d44d31ea0b20f403617d2eaf783d98fdcd90ed1
eastlife <dengbx1212@163.com>
2019-01-19 17:18:48 -0500

    add collaboration function

--- oj-client/src/app/components/editor/editor.component.ts
+++ oj-client/src/app/components/editor/editor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 
+import { ActivatedRoute, Params } from '@angular/router';
+
 declare var ace: any;
 
 @Component({
@@ -14,6 +16,8 @@ export class EditorComponent implements OnInit {
   public languages: string[] = ['Java', 'C++', 'Python', 'JavaScript'];
 
   language = 'Java';
+
+  sessionId: string;
 
   defaultContent = {
     'Java':
@@ -52,22 +56,39 @@ class Solution:
     'JavaScript': 'javascript'
   };
 
-  constructor(@Inject('collaboration') private collaboration) {
+  constructor(@Inject('collaboration') private collaboration,
+              private route: ActivatedRoute) {
 
   }
 
   ngOnInit() {
-    this.initEditor();
+    this.route.params
+      .subscribe(params => {
+        this.sessionId = params['id'];
+        this.initEditor();
+      });
   }
 
   initEditor(): void {
+
     this.editor = ace.edit('editor');
     this.editor.setTheme('ace/theme/eclipse');
     // this.editor.session.setMode('ace/mode/java');
     // this.editor.setValue(this.defaultContent['Java']);
-    this.editor.$blockScrolling = Infinity;
     this.resetEditor();
-    this.collaboration.init();
+    this.editor.$blockScrolling = Infinity;
+
+    document.getElementsByTagName('textarea')[0].focus();
+
+    this.collaboration.init(this.editor, this.sessionId);
+    this.editor.lastAppliedChange = null;
+
+    this.editor.on('change', (e) =>{
+      console.log('editor changes: ' + JSON.stringify(e));
+      if (this.editor.lastAppliedChange !== e) {
+        this.collaboration.change(JSON.stringify(e));
+      }
+    });
   }
 
   setLanguage(language: string): void {
--- oj-server/services/SocketService.js
+++ oj-server/services/SocketService.js
@@ -1,10 +1,46 @@
+// module.exports = function(io) {
+//     io.on('connection', (socket) => {
+//         console.log(socket);
+//
+//         var message = socket.handshake.query['message'];
+//         console.log(message);
+//
+//         io.to(socket.id).emit('message', 'hehe from server');
+//     });
+// };
+
 module.exports = function(io) {
+    var collaborations = [];
+
+    var socketIdToSessionId = [];
+
     io.on('connection', (socket) => {
-        console.log(socket);
 
-        var message = socket.handshake.query['message'];
-        console.log(message);
+        let sessionId = socket.handshake.query['sessionId'];
 
-        io.to(socket.id).emit('message', 'hehe from server');
+        socketIdToSessionId[socket.id] = sessionId;
+
+        if (!(sessionId in collaborations)) {
+            collaborations[sessionId] = {
+                'participants': []
+            };
+        }
+
+        collaborations[sessionId]['participants'].push(socket.id);
+
+        socket.on('change', delta => {
+            console.log('change' + socketIdToSessionId[socket.id] + " " + delta);
+            let sessionId = socketIdToSessionId[socket.id];
+            if (sessionId in collaborations) {
+                let participants = collaborations[sessionId]['participants'];
+                for (let i = 0; i < participants.length; i++) {
+                    if (socket.id != participants[i]){
+                        io.to(participants[i]).emit('change', delta);
+                    }
+                }
+            } else {
+                console.log("WARNING: cannot tie socket_id to any collaboration!");
+            }
+        });
     });
-}
+};
